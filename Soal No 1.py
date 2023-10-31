ecfe49ac764b48fc9f8f1ed8aec005b865c1bab3
naufal554 <141711434+naufal554@users.noreply.github.com>
2023-10-31 20:17:57 +0700

    Add files via upload

---
+++ Soal No 1.py
@@ -0,0 +1,12 @@
+# Harga pizza
+if pesan_pizza == "frankfurter bbq":
+ harga_pizza = 78000
+elif pesan_pizza == "meat monsta":
+ harga_pizza = 76000
+elif pesan_pizza == "super supreme":
+ harga_pizza = 78000
+elif pesan_pizza == "super supreme chicken":
+ harga_pizza = 84000
+else:
+ print("Jenis pizza tidak valid.")
+ exit()
