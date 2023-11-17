2a72d211acf5e762b71893a49004bd705addf9c2
MochAgzhandyRakhaPratama <148831048+rakha-ux@users.noreply.github.com>
2023-11-17 18:55:10 +0700

    Add files via upload

---
+++ Hurdle 4.py
@@ -0,0 +1,52 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def Jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+def Jump_3x():
+    turn_left()
+    move()
+    move()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    move()
+    move()
+    turn_left()
+def Jump_2x():
+    turn_left()
+    move()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    move()
+    turn_left()
+    
+move()
+Jump()
+move()
+Jump_3x()
+Jump()
+Jump_2x()
+Jump()
+move()
+move()
+move()
+move()
+move()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
